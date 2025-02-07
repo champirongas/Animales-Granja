7e1d6c8cbf723f485cb044c0845c464eb9dc10e0
Juan Pablo Ortolá Villanueva <juan.ortola@live.u-tad.com>
2025-02-07 09:53:05 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,53 +1,14 @@
 const express = require('express');
 const _ = require('underscore');
-<<<<<<< HEAD
-
-var port = process.env.PORT || 8080;
-var animals = {
-=======
  
 const port = process.env.PORT || 3000;
 const animals = {
->>>>>>> b292ad3d1d33fc547e0291ef8161333fb9a55c2d
     "cat": "meow",
     "dog": "bark",
     "eel": "hiss",
     "bear": "growl",
     "frog": "croak",
     "lion": "roar",
-<<<<<<< HEAD
-    "bird": "tweet"
-}
-
-function getAnimal() {
-  return animal = _.sample(Object.entries(animals));
-}
-
-const app = express();
-
-app.get('/', function(req, res){
-  const [animal_name, sound] = getAnimal();
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write(`George Orwell had a farm.<br />
-E-I-E-I-O<br />
-And on his farm he had a ${ animal_name }.<br />
-E-I-E-I-O<br />
-With a ${ sound }-${ sound } here.<br />
-And a ${ sound }-${ sound } there.<br />
-Here a ${ sound }, there a ${ sound }.<br />
-Everywhere a ${ sound }-${ sound }.<br />`);
-      res.end();
-});
-
-app.get('/api', function(req, res){
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.write(JSON.stringify(animals));
-  res.end();
-})
-
-module.exports =  app.listen(port, () => {
-  console.log(`Launching server on http://localhost:${ port }`)
-=======
     "bird": "tweet",
     "cow": "moo"
 };
@@ -99,5 +60,4 @@ process.on('SIGTERM', () => {
   server.close(() => {
     console.log('Process terminated');
   });
->>>>>>> b292ad3d1d33fc547e0291ef8161333fb9a55c2d
 });
